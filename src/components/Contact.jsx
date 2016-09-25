f2a2a16f4c3460872c9e9c7701a548cf2ad113cd
Andrew Zey <andrew@andrewzey.com>
2016-09-24 22:33:53 -0700

    Implement Contact Form Logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,37 @@ const propTypes = {
 };
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      _subject: '',
+      message: '',
+      _gotcha: '',
+    };
+  }
+
+  handleChange(event, formFieldName) {
+    this.setState({[formFieldName]: event.target.value});
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log('submitted!');
+    fetch('http://formspree.io/alekseynikolsky+webcontactform@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Accept': 'json',
+      },
+      redirect: 'manual',
+      body: JSON.stringify(this.state),
+    }).then(response => {
+      alert('Your message was sent. Thank you!');
+      this.setState({name: '', email: '', _subject: '', message: ''});
+    }).catch(error => {
+      // TODO: handle error
+      console.error(error);
+    });
   }
 
   render() {
@@ -16,14 +44,39 @@ class Contact extends Component {
       <div>
         <DocumentTitle title='Aleksey Nikolsky - Contact'></DocumentTitle>
           <div><h2>Contact</h2></div>
-          <form id="email-form" onSubmit={this.handleSubmit}>
-            <input tabIndex="1" type="text" name="name" placeholder="Your Name" required />
-            <input tabIndex="2" type="email" name="email" placeholder="Your Email Address" required />
-            <input tabIndex="3" type="text" name="_subject" placeholder="Your Subject Line" required/>
-            <textarea tabIndex="4" name="message" placeholder="Your Message" required />
+          <form id="contact-form" onSubmit={event => this.handleSubmit(event)}>
+            <input
+              tabIndex="1" type="text" name="name" required
+              placeholder="Your Name"
+              value={this.state.name}
+              onChange={event => {this.handleChange(event, 'name')}}
+            />
+            <input
+              tabIndex="2" type="email" name="email" required
+              placeholder="Your Email Address"
+              value={this.state.email}
+              onChange={event => {this.handleChange(event, 'email')}}
+            />
+            <input
+              tabIndex="3" type="text" name="_subject" required
+              placeholder="Your Subject Line"
+              value={this.state._subject}
+              onChange={event => {this.handleChange(event, '_subject')}}
+            />
+            <textarea
+              tabIndex="4" name="message" required
+              placeholder="Your Message"
+              value={this.state.message}
+              onChange={event => {this.handleChange(event, 'message')}}
+            />
             {/*This input field is a honeypot to catch spam bots -->*/}
-            <input tabIndex="5" type="text" name="_gotcha" style={{ display: 'none' }} />
-            <button type="submit" value="Send" >Send</button>
+            <input
+              tabIndex="5" type="text" name="_gotcha"
+              style={{ display: 'none' }}
+              value={this.state._gotcha}
+              onChange={event => {this.handleChange(event, '_gotcha')}}
+            />
+            <button type="submit" value="Send">Send</button>
           </form>
       </div>
     );
