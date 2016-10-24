04efab192ca1c7e11fca3ba5641dbc4866479741
Andrew Zey <andrew@andrewzey.com>
2016-10-24 12:24:52 -0700

    Add ResponsiveVideo component

---
+++ src/components/ResponsiveVideo/ResponsiveVideo.jsx
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react';
+import './ResponsiveVideo.css'
+
+const baseClass = 'ResponsiveVideo';
+
+const propTypes = {
+  src: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+const defaultProps = {
+  src : '',
+  width: 1600,
+  height: 900,
+};
+
+function getRatio(width, height) {
+  return ((parseInt(height, 10) / parseInt(width, 10)) * 100) + '%';
+}
+
+const ResponsiveVideo = ({ src, width, height }) => {
+  const ratio = {
+    position: 'relative',
+    paddingBottom: getRatio(width, height),
+    overflow: 'hidden',
+    maxWidth: '100%',
+    height: 'auto',
+  };
+  return (
+    <div className={`${baseClass}`} style={ratio}>
+      <iframe src={src} width={width} height={height} frameBorder={0} webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+    </div>
+  );
+};
+
+ResponsiveVideo.propTypes = propTypes;
+ResponsiveVideo.defaultProps = defaultProps;
+ResponsiveVideo.baseClass = baseClass;
+
+export default ResponsiveVideo;
