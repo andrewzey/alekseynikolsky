ea3a8588974bcb6e9e257d35e24540d068ba9a38
Andrew Zey <andrew@andrewzey.com>
2016-10-24 10:20:50 -0700

    Fix Navigation Styling Bugs

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -19,49 +19,83 @@ const defaultProps = {
   className: null,
 };
 
-const Navigation = ({
-  pathname,
-  sections,
-  handleSectionClick,
-  hasBackground,
-  bgColor,
-}) => {
-  const navItems = sections.map((section, index) => {
-    const linkUrl = (pathname) ? `${pathname}/${section.url}` : section.url;
-    const shouldMatchExactly = (section.url === '/');
+class Navigation extends React.Component {
+  updateBackgroundClearDivHackHeight() {
+    if (this.backgroundClearDivHack && this.navNode) {
+      this.backgroundClearDivHack.style.height = `${this.navNode.clientHeight + 10}px`;
+    }
+  }
+
+  componentDidMount() {
+    this.updateBackgroundClearDivHackHeight();
+    window.addEventListener('resize', this.updateBackgroundClearDivHackHeight.bind(this));
+  }
+
+  componentDidUpdate() {
+    this.updateBackgroundClearDivHackHeight();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateBackgroundClearDivHackHeight.bind(this));
+  }
+
+  renderBackgroundClearDivHack() {
+    // TODO: make something more elegant for creating space under the background
+    // that is position absolute to span the entire viewport width
+    if (!this.props.hasBackground || !this.props.bgColor) {
+      return null;
+    }
+
+    let height = '65px';
+    if (this.navNode) {
+      height = `${this.navNode.clientHeight}px`;
+    }
     return (
-      <li className={`${baseClass}__nav__item`} key={index}>
-        <Link
-          to={linkUrl}
-          onClick={() => handleSectionClick()}
-          className={`${baseClass}__nav__item__link`}
-          activeClassName={`${baseClass}__nav__item__link--active`}
-          activeOnlyWhenExact={shouldMatchExactly}
-        >
-          {section.displayName}
-        </Link>
-      </li>
+      <div
+        style={{height: height}}
+        ref={(c) => this.backgroundClearDivHack = c }
+      ></div>
     );
-  });
-  const topLevelClass = (hasBackground && bgColor)
-    ? `${baseClass}__bg--${bgColor}` : null;
+  }
 
-  // TODO: make something more elegant for creating space under the background
-  // that is position absolute to span the entire viewport width
-  const backgroundClearDivHack = (hasBackground && bgColor)
-    ? <div style={{height: '65px'}}></div> : null;
+  render() {
+    const navItems = this.props.sections.map((section, index) => {
+      const linkUrl = (this.props.pathname)
+        ? `${this.props.pathname}/${section.url}`
+        : section.url;
+      const shouldMatchExactly = (section.url === '/');
+      return (
+        <li className={`${baseClass}__nav__item`} key={index}>
+          <Link
+            to={linkUrl}
+            onClick={() => this.props.handleSectionClick()}
+            className={`${baseClass}__nav__item__link`}
+            activeClassName={`${baseClass}__nav__item__link--active`}
+            activeOnlyWhenExact={shouldMatchExactly}
+          >
+            {section.displayName}
+          </Link>
+        </li>
+      );
+    });
+    const topLevelClass = (this.props.hasBackground && this.props.bgColor)
+      ? `${baseClass}__bg--${this.props.bgColor}` : null;
 
-  return (
-    <div>
-      <nav className={topLevelClass}>
-        <ul className={`${baseClass}__nav`}>
-          {navItems}
-        </ul>
-      </nav>
-      {backgroundClearDivHack}
-    </div>
-  );
-};
+    return (
+      <div>
+        <nav
+          ref={(c) => this.navNode = c }
+          className={topLevelClass}
+        >
+          <ul className={`${baseClass}__nav`}>
+            {navItems}
+          </ul>
+        </nav>
+        {this.renderBackgroundClearDivHack()}
+      </div>
+    );
+  }
+}
 
 Navigation.propTypes = propTypes;
 Navigation.defaultProps = defaultProps;
