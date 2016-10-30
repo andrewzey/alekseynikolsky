4679a218de26e95e0e904ff08110b40871bcab6e
Andrew Zey <andrew@andrewzey.com>
2016-10-29 17:04:04 -0700

    Update Research copy

--- src/components/Research/Research.jsx
+++ src/components/Research/Research.jsx
@@ -34,8 +34,8 @@ const Research = ({ pathname }) => {
           <HeroOverlay type="research">
             <h3>Research</h3>
             <p>My work is focused on the areas of developmental epistemology of music, emotion, perception of tonal organization, musical synesthesia, and prosody. However, I have always been interested in laying out a comprehensive theory of general music semiotics, similar to “general linguistics” by Ferdinand de Saussure, which would apply to all forms of music.</p>
-            <p>More recently, my attention has gone into investigating the history of popular music, history of so-called “folk music,” and history of development of the music market. I sought to understand whether the state of music today is an aberration of biomusicological “norm” or an organic historical development that only seems to decline in its capacity to communicate information from creator of music to its listener.</p>
-            <p>It was in 2013, that I decided to start publishing some of my findings and solicitingcritical review. I believe that the integration of many disciplines will reveal the formative role that music has played in the cognitive development of humanity.</p>
+            <p>More recently my attention has gone into investigating the history of popular music, history of so-called “folk music,” and history of development of the music market. I sought to understand whether the state of music today is an aberration of biomusicological “norm” or an organic historic development that only seems to decline in its capacity to communicate information from creator of music to its listener.</p>
+            <p>It was in 2013 that I decided to start publishing some of my findings and soliciting critical review. I believe that integration of many disciplines will reveal the formative role that music has played in the cognitive development of humanity.</p>
           </HeroOverlay>
         </Box>
         <Box col={12} sm={12} p={2}>
